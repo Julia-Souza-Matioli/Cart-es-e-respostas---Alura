7ddd5e11c935c34935af6d253a682b3f98f9e24d
Julia-Souza-Matioli <juliasouzamatioli@gmail.com>
2024-11-22 21:40:55 -0300

    lançando novos documentos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Charada',
+    'O que é, o que é: Sempre está na sua frente, mas nunca pode ser visto?',
+    'Futuro HOOOOOOOOOO!!'
+)
+
+criaCartao(
+    'Matématica',
+    'Me explique como 1+1 é igual a dois?',
+    '1 + 1 = 2 porque, em matemática, definimos que adicionar uma unidade (1) a outra unidade (1) resulta em duas unidades (2). É uma regra básica de adição, e o número 2 representa exatamente isso: duas unidades.'
+)
+
+criaCartao(
+    'Opinião pessoal',
+    'Quem é melhor Brino ou T3ddy?',
+    'Não há melhor os dois são muito fofos, são quase irmãos já kkkkk.'
+)
+
+criaCartao(
+    'Matemática',
+    'Por que o livro de matemática se suicidou?',
+    'Porque tinha muitos problemas! KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK '
+)
